d50c2bc70464f2ca5390073c8924142f92f4e90b
s-kinza <shahanaarif81@gmail.com>
2024-10-29 20:08:27 +0500

    update code

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,140 +1,16 @@
-// "use client";
-// import Image from "next/image";
-// import Link from "next/link";
-// import React from "react";
-// import Typewriter from 'typewriter-effect';
-
-// const Hero = () => {
-//   return (
-//     <section className="text-gray-400 bg-gray-900 body-font bg-fixed bg-cover bg-center " id="home">
-//   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-//     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-//       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-//         Hello Coder's! 
-//         <br className="hidden lg:inline-block" />
-        
-    
-    
-//         <br className="hidden lg:inline-block"/>
-//         <Typewriter
-//           options={{
-//             strings: [
-//                 'Myself Syeda Kanzul-Eeman.',
-//               'I AM Web Developer',
-//               'I AM Full Stack Developer',
-//               'I AM Tech Enthusiast',
-//               'I AM Create digital experiences',
-//             ],
-//             autoStart: true,
-//             loop: true,
-//             delay: 75,
-//           }}
-//         />
-//       </h1>
-      
-//       <div className="w-[100px] h-[2px] bg-blue-700"></div>
-      
-//       <p className="mb-8 leading-relaxed">
-//       I'm a passionate coder and tech enthusiast, driven to create seamless and impactful digital experiences. My journey in tech is fueled by curiosity and a dedication to mastering new skills. Let's connect and explore the world of coding together
-//       </p>
-//       <div className="flex justify-center">
-//         <Link href={"#Contact"}>
-//         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-//           Contact
-//         </button>
-//         </Link>
-       
-//       </div>
-//     </div>
-//     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-//       <Image
-//         className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
-//         width={300}
-//         height={300}
-//         alt="hero"
-//         src="/images/pic.png"
-//       />
-//     </div>
-//   </div>
-// </section>
-
-   
-//   );
-// };
-
-// export default Hero;
-// "use client";
-// import Image from "next/image";
-// import Link from "next/link";
-// import React from "react";
-// import Typewriter from 'typewriter-effect';
-
-// const Hero = () => {
-//   return (
-//     <section className="text-gray-400 bg-gray-900 body-font bg-fixed bg-cover bg-center" id="home">
-//       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-//         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-//           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-//             Hello Coder&apos;s! 
-//             <br className="hidden lg:inline-block" />
-            
-//             <br className="hidden lg:inline-block"/>
-//             <Typewriter
-//               options={{
-//                 strings: [
-//                   'Myself Syeda Kanzul-Eeman.',
-//                   'I AM Web Developer',
-//                   'I AM Full Stack Developer',
-//                   'I AM Tech Enthusiast',
-//                   'I AM Create digital experiences',
-//                 ],
-//                 autoStart: true,
-//                 loop: true,
-//                 delay: 75,
-//               }}
-//             />
-//           </h1>
-          
-//           <div className="w-[100px] h-[2px] bg-blue-700"></div>
-          
-//           <p className="mb-8 leading-relaxed">
-//             I&apos;m a passionate coder and tech enthusiast, driven to create seamless and impactful digital experiences. My journey in tech is fueled by curiosity and a dedication to mastering new skills. Let&apos;s connect and explore the world of coding together.
-//           </p>
-//           <div className="flex justify-center">
-//             <Link href={"#Contact"}>
-//               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-//                 Contact
-//               </button>
-//             </Link>
-//           </div>
-//         </div>
-//         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-//           <Image
-//             className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
-//             width={300}
-//             height={300}
-//             alt="hero"
-//             src="/images/pic.png"
-//           />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Hero;
-
 "use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
   return (
-    <section className="text-gray-400 bg-gray-900 body-font bg-fixed bg-cover bg-center" id="home">
+    <section
+      className="text-gray-400 bg-gray-900 body-font bg-fixed bg-cover bg-center"
+      id="home"
+    >
       <div className="container mx-auto flex px-5 py-24 lg:flex-row flex-col items-center">
-        
         {/* Text Section */}
         <div className="flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-10 md:mb-0 items-center text-center">
           <h1 className="title-font text-3xl font-medium text-white mb-4">
@@ -143,11 +19,11 @@ const Hero = () => {
             <Typewriter
               options={{
                 strings: [
-                  'Myself Syeda Kanzul-Eeman.',
-                  'I AM Web Developer',
-                  'I AM Full Stack Developer',
-                  'I AM Tech Enthusiast',
-                  'I CREATE digital experiences',
+                  "Myself Syeda Kanzul-Eeman.",
+                  "I AM Web Developer",
+                  "I AM Full Stack Developer",
+                  "I AM Tech Enthusiast",
+                  "I CREATE digital experiences",
                 ],
                 autoStart: true,
                 loop: true,
@@ -155,13 +31,16 @@ const Hero = () => {
               }}
             />
           </h1>
-          
+
           <div className="w-24 h-[2px] bg-blue-700 mb-6"></div>
-          
+
           <p className="mb-8 leading-relaxed text-base">
-            I&apos;m a passionate coder and tech enthusiast, driven to create seamless and impactful digital experiences. My journey in tech is fueled by curiosity and a dedication to mastering new skills. Let&apos;s connect and explore the world of coding together.
+            I&apos;m a passionate coder and tech enthusiast, driven to create
+            seamless and impactful digital experiences. My journey in tech is
+            fueled by curiosity and a dedication to mastering new skills.
+            Let&apos;s connect and explore the world of coding together.
           </p>
-          
+
           <div className="flex justify-center">
             <Link href={"#Contact"}>
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
@@ -170,7 +49,7 @@ const Hero = () => {
             </Link>
           </div>
         </div>
-        
+
         {/* Image Section */}
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <Image
--- src/components/skill.tsx
+++ src/components/skill.tsx
@@ -1,186 +1,152 @@
-// import React from "react";
-// import { PiFlagCheckeredFill } from "react-icons/pi";
-// const Skill = () => {
-//   return (
-//     <div id="skill">
-//       <section className="text-gray-100 body-font bg-gray-900">
-//         <div className="container px-5 py-24 mx-auto">
-//           <div className="flex flex-col text-center w-full mb-20">
-//             <h2 className="text-indigo-500 tracking-widest font-bold title-font mb-1">
-//               Skills
-//             </h2>
-//             <h1 className="sm:text-3xl text-2xl font-extrabold title-font text-gray-200">
-//               My Skills
-//             </h1>
-//           </div>
-//           <div className="flex flex-wrap -m-4">
-//             {/* Skills  HTML*/}
-//             <div className="p-4 md:w-1/3">
-//               <div className="flex rounded-lg h-full  p-8 flex-col">
-//                 <div className="flex items-center mb-3">
-//                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-//                     <PiFlagCheckeredFill className="text-xl font-bold" />
-//                   </div>
-//                   <h2 className="text-gray-200 text-lg title-font font-extrabold">
-//                     HTML
-//                   </h2>
-//                 </div>
-//                 <div className="flex-grow">
-//                   <div className="relative h-8 w-full bg-gray-300 rounded-full">
-//                     <div className="absolute inset-3 bg-indigo-500 rounded-full"></div>
-//                   </div>
-//                   <p className="text-indigo-500 font-bold text-right">100%</p>
-//                 </div>
-//               </div>
-//             </div>
-//              {/* Skills CSS*/}
-//              <div className="p-4 md:w-1/3">
-//               <div className="flex rounded-lg h-full  p-8 flex-col">
-//                 <div className="flex items-center mb-3">
-//                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-//                     <PiFlagCheckeredFill className="text-xl font-bold" />
-//                   </div>
-//                   <h2 className="text-gray-200 text-lg title-font font-extrabold">
-//                     CSS
-//                   </h2>
-//                 </div>
-//                 <div className="flex-grow">
-//                   <div className="relative h-8 w-full bg-gray-300 rounded-full">
-//                     <div className="absolute inset-3 bg-indigo-500 rounded-full w-[70%]"></div>
-//                   </div>
-//                   <p className="text-indigo-500 font-bold text-right">70%</p>
-//                 </div>
-//               </div>
-//             </div>
-//              {/* Skills JS/TS*/}
-//              <div className="p-4 md:w-1/3">
-//               <div className="flex rounded-lg h-full  p-8 flex-col">
-//                 <div className="flex items-center mb-3">
-//                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-//                     <PiFlagCheckeredFill className="text-xl font-bold" />
-//                   </div>
-//                   <h2 className="text-gray-200 text-lg title-font font-extrabold">
-//                     JavaScript/TypeScript
-//                   </h2>
-//                 </div>
-//                 <div className="flex-grow">
-//                   <div className="relative h-8 w-full bg-gray-300 rounded-full">
-//                     <div className="absolute inset-3 bg-indigo-500 rounded-full w-[50%]"></div>
-//                   </div>
-//                   <p className="text-indigo-500 font-bold text-right">50%</p>
-//                 </div>
-//               </div>
-//             </div>
-//              {/* Skills REACT.JS*/}
-//              <div className="p-4 md:w-1/3">
-//               <div className="flex rounded-lg h-full  p-8 flex-col">
-//                 <div className="flex items-center mb-3">
-//                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-//                     <PiFlagCheckeredFill className="text-xl font-bold" />
-//                   </div>
-//                   <h2 className="text-gray-200 text-lg title-font font-extrabold">
-//                     React.JS
-//                   </h2>
-//                 </div>
-//                 <div className="flex-grow">
-//                   <div className="relative h-8 w-full bg-gray-300 rounded-full">
-//                     <div className="absolute inset-3 bg-indigo-500 rounded-full w-[60%]"></div>
-//                   </div>
-//                   <p className="text-indigo-500 font-bold text-right">60%</p>
-//                 </div>
-//               </div>
-//             </div>
-//             {/* Skills NEXT.JS*/}
-//             <div className="p-4 md:w-1/3">
-//               <div className="flex rounded-lg h-full  p-8 flex-col">
-//                 <div className="flex items-center mb-3">
-//                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-//                     <PiFlagCheckeredFill className="text-xl font-bold" />
-//                   </div>
-//                   <h2 className="text-gray-200 text-lg title-font font-extrabold">
-//                     Next.js
-//                   </h2>
-//                 </div>
-//                 <div className="flex-grow">
-//                   <div className="relative h-8 w-full bg-gray-300 rounded-full">
-//                     <div className="absolute inset-3 bg-indigo-500 rounded-full w-[80%]"></div>
-//                   </div>
-//                   <p className="text-indigo-500 font-bold text-right">80%</p>
-//                 </div>
-//               </div>
-//             </div>
-//              {/* Skills TAILWIND CSS*/}
-//              <div className="p-4 md:w-1/3">
-//               <div className="flex rounded-lg h-full  p-8 flex-col">
-//                 <div className="flex items-center mb-3">
-//                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-//                     <PiFlagCheckeredFill className="text-xl font-bold" />
-//                   </div>
-//                   <h2 className="text-gray-200 text-lg title-font font-extrabold">
-//                     Tailwind CSS
-//                   </h2>
-//                 </div>
-//                 <div className="flex-grow">
-//                   <div className="relative h-8 w-full bg-gray-300 rounded-full">
-//                     <div className="absolute inset-3 bg-indigo-500 rounded-full w-[75%]"></div>
-//                   </div>
-//                   <p className="text-indigo-500 font-bold text-right">75%</p>
-//                 </div>
-//               </div>
-//             </div>
-//             {/* Skills CANVA*/}
-//             <div className="p-4 md:w-1/3">
-//               <div className="flex rounded-lg h-full  p-8 flex-col">
-//                 <div className="flex items-center mb-3">
-//                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-//                     <PiFlagCheckeredFill className="text-xl font-bold" />
-//                   </div>
-//                   <h2 className="text-gray-200 text-lg title-font font-extrabold">
-//                     Canva
-//                   </h2>
-//                 </div>
-//                 <div className="flex-grow">
-//                   <div className="relative h-8 w-full bg-gray-300 rounded-full">
-//                     <div className="absolute inset-3 bg-indigo-500 rounded-full w-[55%]"></div>
-//                   </div>
-//                   <p className="text-indigo-500 font-bold text-right">55%</p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default Skill;
-
 import React from "react";
 import { PiFlagCheckeredFill } from "react-icons/pi";
-
 const Skill = () => {
   return (
     <div id="skill">
       <section className="text-gray-100 body-font bg-gray-900">
-        <div className="container px-5 py-16 md:py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-10 md:mb-20">
-            <h2 className="text-indigo-500 tracking-widest font-bold title-font mb-1 text-lg md:text-xl">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h2 className="text-indigo-500 tracking-widest font-bold title-font mb-1">
               Skills
             </h2>
-            <h1 className="text-2xl sm:text-3xl font-extrabold title-font text-gray-200">
+            <h1 className="sm:text-3xl text-2xl font-extrabold title-font text-gray-200">
               My Skills
             </h1>
           </div>
           <div className="flex flex-wrap -m-4">
-            {/* Skill Card */}
-            <SkillCard name="HTML" progress="100%" width="w-full" />
-            <SkillCard name="CSS" progress="70%" width="w-[70%]" />
-            <SkillCard name="JavaScript/TypeScript" progress="50%" width="w-[50%]" />
-            <SkillCard name="React.JS" progress="60%" width="w-[60%]" />
-            <SkillCard name="Next.js" progress="80%" width="w-[80%]" />
-            <SkillCard name="Tailwind CSS" progress="75%" width="w-[75%]" />
-            <SkillCard name="Canva" progress="55%" width="w-[55%]" />
+            {/* Skills  HTML*/}
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full  p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <PiFlagCheckeredFill className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-200 text-lg title-font font-extrabold">
+                    HTML
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-8 w-full bg-gray-300 rounded-full">
+                    <div className="absolute inset-3 bg-indigo-500 rounded-full"></div>
+                  </div>
+                  <p className="text-indigo-500 font-bold text-right">100%</p>
+                </div>
+              </div>
+            </div>
+             {/* Skills CSS*/}
+             <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full  p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <PiFlagCheckeredFill className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-200 text-lg title-font font-extrabold">
+                    CSS
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-8 w-full bg-gray-300 rounded-full">
+                    <div className="absolute inset-3 bg-indigo-500 rounded-full w-[70%]"></div>
+                  </div>
+                  <p className="text-indigo-500 font-bold text-right">70%</p>
+                </div>
+              </div>
+            </div>
+             {/* Skills JS/TS*/}
+             <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full  p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <PiFlagCheckeredFill className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-200 text-lg title-font font-extrabold">
+                    JavaScript/TypeScript
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-8 w-full bg-gray-300 rounded-full">
+                    <div className="absolute inset-3 bg-indigo-500 rounded-full w-[50%]"></div>
+                  </div>
+                  <p className="text-indigo-500 font-bold text-right">50%</p>
+                </div>
+              </div>
+            </div>
+             {/* Skills REACT.JS*/}
+             <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full  p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <PiFlagCheckeredFill className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-200 text-lg title-font font-extrabold">
+                    React.JS
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-8 w-full bg-gray-300 rounded-full">
+                    <div className="absolute inset-3 bg-indigo-500 rounded-full w-[60%]"></div>
+                  </div>
+                  <p className="text-indigo-500 font-bold text-right">60%</p>
+                </div>
+              </div>
+            </div>
+            {/* Skills NEXT.JS*/}
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full  p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <PiFlagCheckeredFill className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-200 text-lg title-font font-extrabold">
+                    Next.js
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-8 w-full bg-gray-300 rounded-full">
+                    <div className="absolute inset-3 bg-indigo-500 rounded-full w-[80%]"></div>
+                  </div>
+                  <p className="text-indigo-500 font-bold text-right">80%</p>
+                </div>
+              </div>
+            </div>
+             {/* Skills TAILWIND CSS*/}
+             <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full  p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <PiFlagCheckeredFill className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-200 text-lg title-font font-extrabold">
+                    Tailwind CSS
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-8 w-full bg-gray-300 rounded-full">
+                    <div className="absolute inset-3 bg-indigo-500 rounded-full w-[75%]"></div>
+                  </div>
+                  <p className="text-indigo-500 font-bold text-right">75%</p>
+                </div>
+              </div>
+            </div>
+            {/* Skills CANVA*/}
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full  p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <PiFlagCheckeredFill className="text-xl font-bold" />
+                  </div>
+                  <h2 className="text-gray-200 text-lg title-font font-extrabold">
+                    Canva
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <div className="relative h-8 w-full bg-gray-300 rounded-full">
+                    <div className="absolute inset-3 bg-indigo-500 rounded-full w-[55%]"></div>
+                  </div>
+                  <p className="text-indigo-500 font-bold text-right">55%</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -188,26 +154,6 @@ const Skill = () => {
   );
 };
 
-const SkillCard = ({ name, progress, width }) => (
-  <div className="p-4 sm:w-1/2 md:w-1/3 lg:w-1/4">
-    <div className="flex rounded-lg h-full p-6 md:p-8 flex-col">
-      <div className="flex items-center mb-3">
-        <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-          <PiFlagCheckeredFill className="text-lg md:text-xl font-bold" />
-        </div>
-        <h2 className="text-gray-200 text-md md:text-lg title-font font-extrabold">
-          {name}
-        </h2>
-      </div>
-      <div className="flex-grow">
-        <div className="relative h-8 w-full bg-gray-300 rounded-full">
-          <div className={`absolute inset-0 ${width} bg-indigo-500 rounded-full`}></div>
-        </div>
-        <p className="text-indigo-500 font-bold text-right mt-2">{progress}</p>
-      </div>
-    </div>
-  </div>
-);
-
 export default Skill;
+
 

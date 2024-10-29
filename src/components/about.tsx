e85905fc1e9d04df6f310caed56dc0486beb8845
s-kinza <shahanaarif81@gmail.com>
2024-10-29 19:20:04 +0500

    Your commit message here

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -74,6 +74,81 @@
 // };
 
 // export default About;
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+
+// const About = () => {
+//   return (
+//     <div id="about">
+//       <section className="text-gray-300 body-font bg-gray-900 bg-fixed bg-cover bg-center">
+//         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+//           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+//             <Image
+//               className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
+//               alt="hero"
+//               width={300}
+//               height={500}
+//               src="/images/about.png"
+//             />
+//           </div>
+//           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+//             <h1 className="title-font sm:text-4xl text-9xl mb-4 font-extrabold text-gray-100">
+//               About me
+//             </h1>
+//             <p className="mb-7 leading-relaxed">
+//               Hey there! I&apos;m Syeda Kanzul-Eman.
+//               I&apos;m a Full Stack Developer who loves diving into backend development and exploring the endless possibilities of Generative AI 🌐. I focus on creating web solutions that are not only powerful but also efficient and forward-thinking.
+//               <br />
+//               <br />
+//               🌟<b> What I Bring:</b>
+//               <br />
+//               <br />
+//               . Creative Thinker &amp; Tech Enthusiast
+//               <br />
+//               . Passionate Learner
+//               <br />
+//               . Problem Solver for Complex Tech Challenges
+//               <br />
+//               <br />
+//               💼 <b>A Little About Me:</b>
+//               <br />
+//               <br />
+//               I have a strong foundation in full-stack development, with a special interest in integrating Artificial Intelligence into my projects. My goal is simple: to build reliable, high-performance applications that match your unique business needs.
+//               <br />
+//               <br />
+//               🛠 <b>Tech Skills:</b>
+//               <br />
+//               <br />
+//               <b>Languages:</b> TypeScript, JavaScript
+//               <br />
+//               <b>Frameworks:</b> Next.js, Tailwind CSS
+//               <br />
+//               <br />
+//               🤝 <b>Let&apos;s Work Together!</b>
+//               <br />
+//               <br />
+//               I&apos;m always open to new projects, collaborations, and networking opportunities. If you have an idea or a project in mind, let&apos;s connect and make it a reality!
+//               <br />  
+//               <br />
+//               I&apos;m continuously refining my skills to stay in sync with industry trends. At the moment, I&apos;m diving deeper into advanced front-end techniques, blending creativity and functionality to bring ideas to life through code.
+//             </p>
+//             <div className="flex justify-center">
+//               <Link href={"/cv/myCV.pdf"}>
+//                 <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+//                   View CV
+//                 </button>
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default About;
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -82,18 +157,22 @@ const About = () => {
   return (
     <div id="about">
       <section className="text-gray-300 body-font bg-gray-900 bg-fixed bg-cover bg-center">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <div className="container mx-auto flex px-5 py-24 lg:flex-row flex-col items-center">
+          
+          {/* Image Container */}
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 lg:mb-0">
             <Image
-              className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
+              className="object-cover object-center rounded mx-auto w-[300px] h-[500px]"
               alt="hero"
               width={300}
               height={500}
               src="/images/about.png"
             />
           </div>
+          
+          {/* Text Container */}
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-9xl mb-4 font-extrabold text-gray-100">
+            <h1 className="title-font text-4xl mb-4 font-extrabold text-gray-100">
               About me
             </h1>
             <p className="mb-7 leading-relaxed">
@@ -129,7 +208,7 @@ const About = () => {
               <br />
               <br />
               I&apos;m always open to new projects, collaborations, and networking opportunities. If you have an idea or a project in mind, let&apos;s connect and make it a reality!
-              <br />  
+              <br />
               <br />
               I&apos;m continuously refining my skills to stay in sync with industry trends. At the moment, I&apos;m diving deeper into advanced front-end techniques, blending creativity and functionality to bring ideas to life through code.
             </p>
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -97,7 +97,110 @@
 //   );
 // };
 
+// // export default Project;
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+
+// const Project = () => {
+//   return (
+//     <div id="project">
+//       <section className="text-gray-400 bg-gray-900 body-font">
+//         <div className="container px-5 py-24 mx-auto">
+//           <div className="flex flex-col text-center w-full mb-20">
+//             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+//               My Project&apos;s
+//             </h1>
+//           </div>
+//           <div className="flex flex-wrap -m-8">
+//             {/* Project */}
+//             <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
+//               <div className="flex relative">
+//                 <Image
+//                   alt="gallery"
+//                   className="absolute inset-0 w-full h-full object-cover object-center"
+//                   src={"/images/iphone.png"}
+//                   width={800}
+//                   height={800}
+//                 />
+//                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+//                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">
+//                     E-Commerce Project
+//                   </h2>
+//                   <h1 className="title-font text-lg font-medium text-white mb-3">
+//                     I-PHONE 14 PRO MAX
+//                   </h1>
+//                   <p className="leading-relaxed line-clamp-2">
+//                     I built this iPhone website with Next.js and Tailwind CSS.
+//                   </p>
+//                   <Link target="_blank" href={"https://i-phone-store-e-commerce-website-5i7k.vercel.app"}>
+//                     <p className="leading-relaxed text-indigo-400 hover:underline">View Project:</p>
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//             {/* Project */}
+//             <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
+//               <div className="flex relative">
+//                 <Image
+//                   alt="gallery"
+//                   className="absolute inset-0 w-full h-full object-cover object-center"
+//                   src={"/images/fb.png"}
+//                   width={800}
+//                   height={800}
+//                 />
+//                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+//                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">
+//                     One Page Project
+//                   </h2>
+//                   <h1 className="title-font text-lg font-medium text-white mb-3">
+//                     FACEBOOK CLONE PAGE
+//                   </h1>
+//                   <p className="leading-relaxed line-clamp-2">
+//                     I built this Facebook Clone page with Next.js and Tailwind CSS.
+//                   </p>
+//                   <Link target="_blank" href={"https://next-js-facebook-page-clone.vercel.app"}>
+//                     <p className="leading-relaxed text-indigo-400 hover:underline">View Project:</p>
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//             {/* Project */}
+//             <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
+//               <div className="flex relative">
+//                 <Image
+//                   alt="gallery"
+//                   className="absolute inset-0 w-full h-full object-cover object-center"
+//                   src={"/images/shopping.png"}
+//                   width={300}
+//                   height={300}
+//                 />
+//                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+//                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">
+//                     E-Commerce Project
+//                   </h2>
+//                   <h1 className="title-font text-lg font-medium text-white mb-3">
+//                     Shopping Website
+//                   </h1>
+//                   <p className="leading-relaxed line-clamp-2">
+//                     I built this E-Commerce Shopping Website with Next.js and Tailwind CSS.
+//                   </p>
+//                   <Link target="_blank" href={"https://e-commerce-shopping-website-albz.vercel.app"}>
+//                     <p className="leading-relaxed text-indigo-400 hover:underline">View Project:</p>
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// };
+
 // export default Project;
+
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -106,16 +209,16 @@ const Project = () => {
   return (
     <div id="project">
       <section className="text-gray-400 bg-gray-900 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
-              My Project&apos;s
+        <div className="container px-5 py-16 md:py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-10 md:mb-20">
+            <h1 className="text-2xl sm:text-3xl font-medium title-font mb-4 text-white">
+              My Projects
             </h1>
           </div>
-          <div className="flex flex-wrap -m-8">
+          <div className="flex flex-wrap -m-4">
             {/* Project */}
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
-              <div className="flex relative">
+            <div className="w-full md:w-1/2 lg:w-1/3 p-4 cursor-pointer">
+              <div className="flex relative h-64 md:h-72 lg:h-80">
                 <Image
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
@@ -140,8 +243,8 @@ const Project = () => {
               </div>
             </div>
             {/* Project */}
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
-              <div className="flex relative">
+            <div className="w-full md:w-1/2 lg:w-1/3 p-4 cursor-pointer">
+              <div className="flex relative h-64 md:h-72 lg:h-80">
                 <Image
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
@@ -166,14 +269,14 @@ const Project = () => {
               </div>
             </div>
             {/* Project */}
-            <div className="lg:w-1/2 sm:w-1/2 p-4 cursor-pointer">
-              <div className="flex relative">
+            <div className="w-full md:w-1/2 lg:w-1/3 p-4 cursor-pointer">
+              <div className="flex relative h-64 md:h-72 lg:h-80">
                 <Image
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={"/images/shopping.png"}
-                  width={300}
-                  height={300}
+                  width={800}
+                  height={800}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">
